31a579abe4b173784b1ff16b76ca6c1311f2fcde
misselvexu <iskp.me@gmail.com>
2021-05-12 00:55:14 +0800

    fix startup.sh agent service name issues

--- bootstrap/migration/src/main/bin/startup.sh
+++ bootstrap/migration/src/main/bin/startup.sh
@@ -76,7 +76,7 @@ JAVA_OPT="${JAVA_OPT} -Dspring.profiles.active=${ENV}"
 ## check skywalking config
 if [[ ! "${ENV}" == "default" ]]; then
     # enabled monitor
-    JAVA_OPT="${JAVA_OPT} -javaagent:/data/skywalking-agent/skywalking-agent.jar -Dskywalking.agent.service_name=mixmicro-ingress-server"
+    JAVA_OPT="${JAVA_OPT} -javaagent:/data/skywalking-agent/skywalking-agent.jar -Dskywalking.agent.service_name=mixmicro-ingress-bootstrap"
 fi
 
 JAVA_OPT="${JAVA_OPT} -XX:-OmitStackTraceInFastThrow"
